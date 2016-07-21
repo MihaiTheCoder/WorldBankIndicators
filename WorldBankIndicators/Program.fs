d51c8d3e800fc2d8bf8fc45351a9b7d1156e43e4
Petrutiu Mihai <mihai.petrutiu@qubiz.com>
2016-07-21 18:52:19 +0300

    Add Y axis description to understand the title

--- WorldBankIndicators/Program.fs
+++ WorldBankIndicators/Program.fs
@@ -5,28 +5,29 @@ type WorldBank = WorldBankDataProvider<"World Development Indicators", Asynchron
 let wb = WorldBank.GetDataContext()
 let countries = [| wb.Countries.Bulgaria; wb.Countries.Hungary; wb.Countries.Serbia; wb.Countries.Ukraine; wb.Countries.Greece; wb.Countries.Romania |]
 
-let draw (indicatorForEachCountry: Runtime.WorldBank.Indicator list, title) = 
+let draw (indicatorForEachCountry: Runtime.WorldBank.Indicator list, title, yAxisTitle) = 
     indicatorForEachCountry 
         |> List.map (fun i -> Chart.Line(i, i.Code))
         |> Chart.Combine
         |> Chart.WithLegend true
         |> Chart.WithTitle(title)
         |> Chart.WithXAxis(Title="Year", Min=1990.0, Max=2016.0)
+        |> Chart.WithYAxis(Title=yAxisTitle)
         |> Chart.Show
 
 [<EntryPoint>]
 let main argv = 
     let dieselPumpPrice = [ for c in countries -> c.Indicators.``Pump price for diesel fuel (US$ per liter)`` ]
     
-    draw(dieselPumpPrice, "Diesel pump price")   
+    draw(dieselPumpPrice, "Diesel pump price", "US$")   
 
     let gdpPerCapita = [ for c in countries -> c.Indicators.``GDP per capita (current US$)`` ]
     
-    draw(gdpPerCapita, "GDB Per capita")
+    draw(gdpPerCapita, "GDB Per capita", "US$")
 
     let gdpPerCapita = [ for c in countries -> c.Indicators.``Birth rate, crude (per 1,000 people)`` ]
     
-    draw(gdpPerCapita, "Birth rate per 1000 people")    
+    draw(gdpPerCapita, "Birth rate year/1000 people", "birth rate in one year for 1000 people")    
     
     printfn "%A" argv
     0 // return an integer exit code
